e05d0a2cacb20c35ab0151e9577a8c18af2b58bc
Jeremy <boiselet.jeremy@gmail.com>
2017-08-04 15:14:05 +0200

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  facebook_api_key: '346797825733834',
-  facebook_api_secret: '0909e9116ad9b66851ff34846e2a07c0',
+  facebook_api_key: FACEBOOK_API_KEY,
+  facebook_api_secret: FACEBOOK_API_KEY_SECRET,
   callback_url: 'http://localhost:3000/facebook/callback'
 };
